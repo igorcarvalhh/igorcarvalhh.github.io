28a35506c9a9dba7cb49c2fc02cf63053f1cdf3f
Igor Carvalho <igorcarvalhh@gmail.com>
2021-10-27 16:52:52 -0300

    deploy 2.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Home, Container } from './style';
+import Typewriter from 'typewriter-effect';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container>
+      <Home>
+        <h3>HI THERE👋,I’M IGOR</h3>
+        <div>
+        <Typewriter
+          options={{
+            strings: ['Turn your ideas into Reallity.'],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+        </div>
+        <h2>DESIGNER + DEVELOPER👨‍💻</h2>  
         <p>
-          Página em construção. Novidades em breve!
+          I’m a software engineer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I’m focused on 
+          building accessible, human-centered products at Upstatement.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <button>CONTACT ME</button>
+      </Home>
+    </Container>
   );
 }
 
---
+++ src/style.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import Typewriter from 'typewriter-effect';
+
+export const Container = styled.div`
+    min-width: 880px;
+`;
+
+export const Home = styled.div`
+    margin: 60px 17% ;
+    text-align: left;
+    h3 { 
+        font-size: 24px;
+        font-weight: normal;
+    }
+
+    div {
+        
+        height: 140px;
+        margin-top : 36px;
+    }
+
+    
+
+    ${Typewriter} { 
+        font-size: 4rem;
+        font-weight: 900;
+    }
+
+    h2 {
+        color: #05F324;
+        font-size: 1.5rem;
+        font-weight: normal;
+        letter-spacing: 5px;
+        margin-top: 40px;
+    }
+
+    p {
+        font-size: 14px;
+        width: 600px;
+        margin-top: 30px;
+        font-weight:  normal;
+    }
+
+    button {
+        background-color: #FFD700;
+        border: none;
+        padding: 20px 40px;
+        border-radius: 5px;
+        font-weight: 800;
+        font-size: 18px;
+        font-family: "JetBrainsMono NF";
+        margin-top: 40px;
+    }
+`
